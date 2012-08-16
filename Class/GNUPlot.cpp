5c77952dfaf98cc43ddfbeed9481195ec7c41f07
marian0 <marianosantafe@gmail.com>
2012-08-16 18:34:02 -0300

    Se creo la clase para graficar GNUPlot necesary

--- Class/GNUPlot.cpp
+++ Class/GNUPlot.cpp
@@ -1,52 +1,28 @@
+#include "GNUPlot.h"
+
+
 /*
+        Class: GNUPlot 
 
-Clase para Plotear..... Gracias a GNU PLOT
-
-Para instalar el gnu plot es bajarlo, ./configure , sudo make install
-Necesita incluirse #include <stdio.h>
-
-
-algunos ejemplos
-        plotter("set pointsize 3");
-        plotter("set grid back");
-        
-        plotter("set xzeroaxis lt -1");
-        plotter("set yzeroaxis lt -1");
-        
-        plotter("set xrange [-2:2]");
-        plotter("set yrange [-2:2]");
-        //plotter("set key box"); es un recuadro al titulo derecho
-        plotter("set multiplot");
-        plotter("set title \"Grafica TP 1 - Inteligencia Computacional 2011\"");
-        plotter("plot \"-\" notitle pt 6");
-
-
-        this->plot("plot " + doubletostr(W[0]/W[2]) + "-" + doubletostr(W[1]/W[2]) + "*x lt "+ doubletostr((int)rand()%30+1) +" notitle");
+        Abre un pipe para graficación en GNUPlot desde C++
 */
-class GNUPlot {
-public:
-        GNUplot() throw(string);
-        ~GNUplot();
-        void operator ()(const string& command);
-        // send any command to gnuplot
-protected:
-        FILE *gnuplotpipe;
-};
 
-GNUplot::GNUplot() throw(string) {
-        gnuplotpipe=popen("gnuplot","w");
-        if (!gnuplotpipe) {
-                throw("Gnuplot not found !");
-        }
+
+GNUPlot::GNUPlot() throw(std::string) {
+	//persist para evitar que se cierre la ventana
+    gnuplotpipe = popen("gnuplot -persist","w"); 
+    if (!gnuplotpipe) {
+        throw("Gnuplot not found !");
+    }
 }
 
-GNUplot::~GNUplot() {
-        fprintf(gnuplotpipe,"exit\n");
-        pclose(gnuplotpipe);
+GNUPlot::~GNUPlot() {
+    fprintf(gnuplotpipe,"exit");
+    pclose(gnuplotpipe);
 }
 
-void GNUplot::operator() (const string& command) {
-        fprintf(gnuplotpipe,"%s\n",command.c_str());
-        fflush(gnuplotpipe);
-        // flush is necessary, nothing gets plotted else
+void GNUPlot::operator() (const std::string& command) {
+    fprintf(gnuplotpipe,"%s\n",command.c_str());
+    fflush(gnuplotpipe);
+    // flush is necessary, nothing gets plotted else
 };
--- TP1_Perceptron/main.cpp
+++ TP1_Perceptron/main.cpp
@@ -4,11 +4,18 @@
 //#include "Neurona.h"
 #include "utils.cpp"
 
+#include "GNUPlot.h"
 
 int main (int argc, char *argv[]) {
-	std::vector<std::vector<double> > entrada;
-	parseCSV("input.txt", entrada);
-	printVectorVector<double>(entrada);
+	GNUPlot plotter;
+	
+	plotter("plot sin(x); pause -1");
+	
+	
+//	
+//	std::vector<std::vector<double> > entrada;
+//	parseCSV("input.txt", entrada);
+//	printVectorVector<double>(entrada);
 
 	return 0;
 }
