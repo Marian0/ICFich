a8d7aed8cec6ec15c841a761712161493bba3cf9
marian0 <marianosantafe@gmail.com>
2012-09-19 10:30:37 -0300

    Fix de NeuronaRBF.h

--- Class/NeuronaRBF.h
+++ Class/NeuronaRBF.h
@@ -21,21 +21,16 @@ public:
     
 	//Constructor
 	//  * dim = Cantidad de entradas
-	Neurona(unsigned int dim, float min = -0.5, float max= 0.5, unsigned int funcion = 1, float constante_aprendizaje = 0.5);
+	NeuronaRBF(unsigned int dim);
 
 	// Devuelve la respuesta de la Neurona en base a la entrada especificada por parámetro	
-	float getResponse(std::vector<float> X, float parametro_sigmoidea);
+	float getResponse(std::vector<float> X);
 
 	//Obtiene por referencia el vector W
-	std::vector<float> getW();
+	std::vector<float> getMu();
 	//Define por referencia el vector W
 	void setW(std::vector<float> & W);
 	
-	//Devuelve la constante de aprendizaje
-	float getConstanteAprendizaje();
-
-	//Obtiene por referencia el vector W
-	std::vector<float> getWn_1();
 };
 
 #endif
